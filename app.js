567d0bb4098b9970c33c862b8687596a5556a199
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-17 11:31:15 +0100

    Ajout de l'intro Météo

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let btnChearch = document.getElementById('btnEnter');
 let divResume = document.getElementById("resume");
 
 btnChearch.addEventListener('click', ()=> {
+    let features = "menuba=no, scrollbars=no, width=600, height=500, status=no, toolbar=no";
+    myWindow = window.open("TF1 Météo Intro [720p] [2020] [FR].mp4", '', features);
     let city = document.getElementById("input").value;
     let requesturl = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&lang=fr&appid=4711dd873ab37e925e454dd1a9138fc0";
     let xhr = new XMLHttpRequest();
@@ -18,10 +20,9 @@ btnChearch.addEventListener('click', ()=> {
         divResume.innerHTML += "Min : " + Math.ceil(response.main.temp_min) + "°C " + " Max : " + Math.ceil(response.main.temp_max)
             + "°C, " + "ressentie : " + Math.ceil(response.main.feels_like) + "°C" + "<br>";
         divResume.innerHTML += "<br>" + response.weather[0].description + "<br>";
-        divResume.innerHTML += "Humidité : " + response.main.humidity +"%" + "<br>";
+        divResume.innerHTML += "Humidité : " + response.main.humidity +" %" + "<br>";
         divResume.innerHTML += "Vent : " + response.wind.speed + " km/h";
     }
-
     xhr.send();
 })
 
