73c9f3864d3a8dfc30e28913ad075b6637e57fe0
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-17 09:47:19 +0100

    petit comit pour etre sur

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&lang=fr&appid=4711dd873ab37e925e454dd1a9138fc0
+let btnChearch = document.getElementById('btnEnter');
+
+btnChearch.addEventListener('click', ()=> {
+    let divResume = document.getElementById("resume");
+    let city = document.getElementById("input").value;
+    let requesturl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&lang=fr&appid=4711dd873ab37e925e454dd1a9138fc0";
+    let xhr = new XMLHttpRequest();
+})
+
