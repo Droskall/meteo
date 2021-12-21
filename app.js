f86696a30e2dd49672d655ea8a3da6eaa9360bf0
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-21 14:38:13 +0100

    finititi finitoto

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
-let btnChearch = document.getElementById('btnEnter');
-let divResume = document.getElementById("resume");
+let btnChearch = $('#btnEnter');
+let divResume = $("#resume");
 
-btnChearch.addEventListener('click', ()=> {
-    let features = "menuba=no, scrollbars=no, width=600, height=500, status=no, toolbar=no";
-    myWindow = window.open("TF1 Météo Intro [720p] [2020] [FR].mp4", '', features);
+btnChearch.on('click', ()=> {
 
-    let city = document.getElementById("input").value;
-    let requesturl = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&lang=fr&appid=4711dd873ab37e925e454dd1a9138fc0";
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", requesturl);
-    xhr.responseType = "json";
+    let city = $("#input").val();
 
-    xhr.onload = () => {
-        if (xhr.status !== 200){
-        }
-        let response = xhr.response;
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&lang=fr&appid=4711dd873ab37e925e454dd1a9138fc0",
+        method: "GET",
+        dataType: "json"
+    })
 
-        divResume.innerHTML += city + "<br>";
-        divResume.innerHTML += "<br>" + "Température : " + Math.ceil(response.main.temp)+"°C" + "<br>";
-        divResume.innerHTML += "Min : " + Math.ceil(response.main.temp_min) + "°C " + " Max : " + Math.ceil(response.main.temp_max)
-            + "°C, " + "ressentie : " + Math.ceil(response.main.feels_like) + "°C" + "<br>";
-        divResume.innerHTML += "<br>" + response.weather[0].description + "<br>";
-        divResume.innerHTML += "Humidité : " + response.main.humidity +" %" + "<br>";
-        divResume.innerHTML += "Vent : " + response.wind.speed + " km/h" + "<br>";
-    }
-    xhr.send();
+        .done(function (response){
+            divResume.html(city + "<br>" + "<br>" + "Température : " + Math.ceil(response.main.temp)+"°C" + "<br>"
+            + "Min : " + Math.ceil(response.main.temp_min) + "°C " + " Max : " + Math.ceil(response.main.temp_max)
+                + "°C, " + "ressentie : " + Math.ceil(response.main.feels_like) + "°C" + "<br>" + "<br>"
+                + response.weather[0].description + "<br>" + "Humidité : " + response.main.humidity +" %" + "<br>" +
+                "Vent : " + response.wind.speed + " km/h" + "<br>"
+            );
+        })
 });
 
-let btnReset = document.getElementById('reset');
-btnReset.addEventListener('click', () => {
+let btnReset = $('#reset');
+btnReset.on('click', () => {
     location.reload();
-})
-
+});
